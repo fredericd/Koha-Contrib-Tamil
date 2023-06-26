459c86ca9b2b97c6b5cad9246b57512102490019
Frédéric Demians <f.demians@tamil.fr>
2023-06-26 15:03:31 +0200

    koha-dump-biblio pour 22.11 et supérieur

--- lib/Koha/Contrib/Tamil/Biblio/Dumper.pm
+++ lib/Koha/Contrib/Tamil/Biblio/Dumper.pm
@@ -109,9 +109,7 @@ before 'run' => sub {
     $self->sth($sth);
 
     $self->sth_marcxml( $self->koha->dbh->prepare(
-        $self->koha->_old_marc_biblio_sub
-        ? "SELECT marcxml FROM biblioitems WHERE biblionumber=?"
-        : "SELECT metadata FROM biblio_metadata WHERE biblionumber=?"
+        "SELECT metadata FROM biblio_metadata WHERE biblionumber=?"
     ) );
 
     $query = "SELECT * FROM items WHERE biblionumber=?";
